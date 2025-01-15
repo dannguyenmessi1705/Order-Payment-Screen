71b972afcdf5bc44920dff8fc46eecebe09249c8
dannd1 dannd1 <dannd1@viettel.com.vn>
2025-01-15 16:00:11 +0700

    fix async

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import { PaymentStatus } from "@/components/PaymentStatus"
+import { PaymentStatus } from '@/components/PaymentStatus'
 
-export default function Home({
-                               searchParams,
-                             }: {
-  searchParams: { [key: string]: string | string[] | undefined }
+export default async function Home({
+                                     searchParams,
+                                   }: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
+  const resolvedSearchParams = await searchParams;
+
   return (
       <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 bg-gradient-to-br from-gray-100 to-gray-200">
-        <PaymentStatus searchParams={searchParams} />
+        <PaymentStatus searchParams={resolvedSearchParams} />
       </main>
   )
 }
+
